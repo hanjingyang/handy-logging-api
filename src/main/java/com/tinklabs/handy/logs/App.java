eacdcab2636cda52fea117e25024c4499c15a79c
lyon <admin@example.com>
2019-03-20 16:13:46 +0800

    GUTS-6490#修改springboot配置

--- src/main/java/com/tinklabs/handy/logs/App.java
+++ src/main/java/com/tinklabs/handy/logs/App.java
@@ -2,8 +2,14 @@ package com.tinklabs.handy.logs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.tinklabs.handy.**.base",
+        "com.tinklabs.handy.**.controller",
+        "com.tinklabs.handy.**.service",
+        "com.tinklabs.handy.**.mapper",
+        "com.tinklabs.handy.*"})
 public class App {
 
 	public static void main(String[] args) {
