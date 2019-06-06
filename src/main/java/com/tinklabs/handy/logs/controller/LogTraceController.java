7f4f3682e2cea538ee39e473d33d50c19c7eabb6
hanjingyang <115877264@qq.com>
2019-06-06 14:56:32 +0800

    kafka rest message base64 encode

--- src/main/java/com/tinklabs/handy/logs/controller/LogTraceController.java
+++ src/main/java/com/tinklabs/handy/logs/controller/LogTraceController.java
@@ -1,6 +1,7 @@
 package com.tinklabs.handy.logs.controller;
 
 import com.tinklabs.handy.logs.constants.TopicType;
+import com.tinklabs.handy.logs.utils.Base64Utils;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -51,7 +52,7 @@ public class LogTraceController {
 	 * @company: tinklabs
 	 * @author: pengtao
 	 * @date: 2019 2019年3月25日 下午5:57:55
-	 * @param topic
+	 * @param
 	 * @param data
 	 * @return
 	 */
@@ -88,7 +89,7 @@ public class LogTraceController {
 	@RequestMapping(value = "rest", method = RequestMethod.POST)
 	public ResultVO sendRestTraceLog( @RequestBody Map<String, Object> data,
 			@RequestParam("topic") String topic) {
-		logger.debug("topic=" + topic);
+		logger.debug("sendRestTraceLog topic=" + topic);
 		if (data == null || data.isEmpty()) {
 			// 如果提交参数为空，返回错误
 			logger.debug("request body parameter empty.");
@@ -97,7 +98,8 @@ public class LogTraceController {
 		// 转换消息为json文本
 		String dataStr = JSONUtil.toJsonStr(data);
 		logger.info("recived data: " + dataStr);
-		//转换成服务器接收的格式 {"records":[{"value":"5bCK5pWs55qE5a6i5oi35oKo5aW977yMaGkga2Fma2EsIGknbSB4bmNoYWxs"}]} 
+		//转换成服务器接收的格式 {"records":[{"value":"5bCK5pWs55qE5a6i5oi35oKo5aW977yMaGkga2Fma2EsIGknbSB4bmNoYWxs"}]}
+		dataStr = Base64Utils.encode(dataStr);
 		//屏蔽了批量发送，一次只发送一条
 		Map<String, List<Map<String, String>>> records = new HashMap<>();
 		List<Map<String, String>> valueList = new ArrayList<>();
